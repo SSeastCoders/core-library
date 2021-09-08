45ff25b5999cb5139eebaf0002ad0e258acc006a
Amanda R <amanda@Amandas-MacBook-Pro.local>
2021-09-08 16:19:51 -0400

    updated

--- src/main/java/com/ss/eastcoderbank/core/repository/UserRepository.java
+++ src/main/java/com/ss/eastcoderbank/core/repository/UserRepository.java
@@ -3,19 +3,39 @@ package com.ss.eastcoderbank.core.repository;
 import com.ss.eastcoderbank.core.model.user.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import org.springframework.stereotype.Repository;
 
 
-@Repository
-public interface UserRepository extends JpaRepository <User, Integer> {
+    @Repository
+    public interface UserRepository extends JpaRepository <User, Integer> {
 
 
-    User findUsersByEmailOrCredentialUsername(String email, String Username);
+        User findUsersByEmailOrCredentialUsername(String email, String Username);
 
-    Page<User> findUserByRoleTitle(String title, Pageable page);
+        Page<User> findUserByRoleTitle(String title, Pageable page);
 
 
-    User findByCredentialUsername(String username);
+        User findByCredentialUsername(String username);
 
-}
+        /**
+         *
+         * @param page
+         * @return a Page of active users
+         */
+        Page<User> findAllByActiveStatusIsTrue(Pageable page);
+
+        /**
+         *
+         * @param page
+         * @return a Page of inactive users
+         */
+        Page<User> findAllByActiveStatusIsFalse(Pageable page);
+
+
+    }
+
+
+
