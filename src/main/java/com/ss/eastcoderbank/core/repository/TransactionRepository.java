bd7040560038ee8ef1cc969478664470545f4e9b
asernass <alejandro.serna@smoothstack.com>
2021-09-21 13:27:20 -0400

    added filtering options for database

--- src/main/java/com/ss/eastcoderbank/core/repository/TransactionRepository.java
+++ src/main/java/com/ss/eastcoderbank/core/repository/TransactionRepository.java
@@ -5,6 +5,13 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+
 public interface TransactionRepository extends JpaRepository<Transaction, Integer> {
-    Page<Transaction> findTransactionByAccountId(Integer id, Pageable pageable);
+    Page<Transaction> findTransactionByAccountIdAndDescriptionContainingAndDateBetweenAndAmountBetween(Integer id, String search, LocalDate start, LocalDate end, Float fromAmount, Float toAmount, Pageable pageable);
+    Page<Transaction> findTransactionByAccountIdAndDescriptionContaining(Integer id, Pageable pageable, String search);
+
+    default Page<Transaction> findTransactionByOptions(Integer id, String search, LocalDate start, LocalDate end, Float fromAmount, Float toAmount, Pageable pageable) {
+        return findTransactionByAccountIdAndDescriptionContainingAndDateBetweenAndAmountBetween(id, search, start, end, fromAmount, toAmount, pageable);
+    }
 }
