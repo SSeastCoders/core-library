9923cf967a604fe866cdc96fe987e20241e126d5
asernass <alejandro.serna@smoothstack.com>
2021-11-16 20:05:29 -0500

    account filtering added

--- src/main/java/com/ss/eastcoderbank/core/repository/AccountRepository.java
+++ src/main/java/com/ss/eastcoderbank/core/repository/AccountRepository.java
@@ -1,9 +1,15 @@
 package com.ss.eastcoderbank.core.repository;
 
 import com.ss.eastcoderbank.core.model.account.Account;
+import com.ss.eastcoderbank.core.model.account.AccountType;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 
@@ -15,4 +21,13 @@ public interface AccountRepository extends JpaRepository<Account, Integer> {
      * @return list of accounts
      */
     List<Account> findAccountByUsersId(Integer id);
+
+    @Query("select a from Account a where a.nickName like %:nickName% " +
+            "and (:fromDate is null or a.openDate >= :fromDate) and " +
+            "(:toDate is null or a.openDate <= :toDate) and (:fromAmount is null or a.balance >= :fromAmount) " +
+            "and (:toAmount is null or a.balance <= :toAmount) and" +
+            "(:type is null or a.accountType = :type) and (:status is null or a.activeStatus = :status)")
+    Page<Account> findAccountsByFilter(@Param("nickName") String nickName, @Param("fromDate") LocalDate fromDate, @Param("toDate") LocalDate toDate,
+                                       @Param("fromAmount") Float fromAmount, @Param("toAmount") Float toAmount, @Param("type") AccountType type,
+                                       @Param("status") Boolean status, Pageable pageable);
 }
