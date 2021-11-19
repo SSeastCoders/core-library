97e1f8ec162a9617cb545f6b3da41b6be3764eee
asernass <alejandro.serna@smoothstack.com>
2021-11-19 16:35:19 -0500

    added card filtering

--- src/main/java/com/ss/eastcoderbank/core/repository/CreditRepository.java
+++ src/main/java/com/ss/eastcoderbank/core/repository/CreditRepository.java
@@ -2,9 +2,14 @@ package com.ss.eastcoderbank.core.repository;
 
 import com.ss.eastcoderbank.core.model.card.CreditCard;
 import com.ss.eastcoderbank.core.model.user.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,4 +18,13 @@ public interface CreditRepository extends JpaRepository<CreditCard, Integer> {
     List<CreditCard> findCreditCardsByUsersIn(List<User> users);
 
     Optional<CreditCard> findCreditCardsBySwipe(String swipe);
+
+    @Query("select a from CreditCard a where a.nickName like %:nickName% " +
+            "and (:fromDate is null or a.openDate >= :fromDate) and " +
+            "(:toDate is null or a.openDate <= :toDate) and (:fromAmount is null or a.balance >= :fromAmount) " +
+            "and (:toAmount is null or a.balance <= :toAmount) and" +
+            "(:status is null or a.activeStatus = :status)")
+    Page<CreditCard> findCreditCardByFilter(@Param("nickName") String nickName, @Param("fromDate") LocalDate fromDate, @Param("toDate") LocalDate toDate,
+                                       @Param("fromAmount") Float fromAmount, @Param("toAmount") Float toAmount,
+                                       @Param("status") Boolean status, Pageable pageable);
 }
